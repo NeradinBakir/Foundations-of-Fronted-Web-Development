a39a159ef202af6ee7f9f86e5eb063d6b364ebad
Bakir Neradin <bakirneradin11@gmail.com>
2024-05-29 15:59:09 +0200

    5

--- project2/pr2.js
+++ project2/pr2.js
@@ -1,39 +1,40 @@
 function openMenu(evt, menuName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("menu");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
-    }
-    document.getElementById(menuName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  
-  document.getElementById("myLink").click();
-
-function zoomIn(element) {
-  element.style.transform = "scale(1.1)";
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
 }
 
-function zoomOut(element) {
-  element.style.transform = "scale(1)";
+function openModal(imageUrl) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+  modal.style.display = "block";
+  modalImg.src = imageUrl;
 }
 
-function myFunction() {
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function Fuunction() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "Adress:") {
-    x.innerHTML = "Restaurant adress:Garovci bb , Bakery adress: Hadželi 80, Hadžići";
+    x.innerHTML = "Restaurant adress: Garovci bb, Bakery adress: Hadželi 80, Hadžići";
   } else {
-    x.innerHTML = "Restaurant adress:Garovci bb , Bakery adress: Hadželi 80, Hadžići";
+    x.innerHTML = "Adress:";
   }
 }
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
+var acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
@@ -41,10 +42,54 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
 
+function toggleText() {
+  var additionalInfo = document.getElementById("additional-info");
+  var btn = document.getElementById("load-more-btn");
+  if (additionalInfo.style.display === "none") {
+    additionalInfo.style.display = "block";
+    btn.textContent = "Show Less";
+  } else {
+    additionalInfo.style.display = "none";
+    btn.textContent = "Load More";
+  }
+}
 
-  
-  
+//const themeSwitch = document.getElementById('theme-switch');
+
+//themeSwitch.addEventListener('change', () => {
+ // if (themeSwitch.checked) {
+ //   document.body.classList.add('dark-theme');
+  //  document.body.classList.remove('light-theme');
+ // } else {
+ //   document.body.classList.remove('dark-theme');
+ //   document.body.classList.add('light-theme');
+ // }
+//});
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+$(document).ready(function() {
+  $('#saveChangesButton').click(function() {
+    var reviewerName = $('#reviewerName').val();
+    var reviewText = $('#reviewText').val();
+
+    console.log('Reviewer Name:', reviewerName);
+    console.log('Review Text:', reviewText);
+
+    // Ovdje možete dodati logiku za slanje podataka na server ili daljnju obradu
+
+   $('#exampleModal').modal('hide'); // Zatvori modal nakon što se podaci ispišu
+  });
+
+  $("#deleteButton").click(function () {
+    console.clear();
+  })
+
+});
